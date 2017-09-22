b46268aa1802d8288d7ed9b2ee003cd254959cee
Brian Chang <kyoukamu@gmail.com>
2017-09-21 20:45:07 -0700

    added show/edit/delete options with sql querys, allowing user to edit the information displayed

--- server.py
+++ server.py
@@ -8,14 +8,17 @@ mysql=MySQLConnector(app,'fullfriends')
 
 @app.route('/')
 def index():
-	query = "SELECT friends.name, friends.age, DATE_FORMAT(friends.join_date,'%M %d') AS join_date, DATE_FORMAT(friends.join_date, '%Y') AS year FROM friends"
+	query = "SELECT friends.id, friends.name, friends.age, DATE_FORMAT(friends.join_date,'%M %d') AS join_date, DATE_FORMAT(friends.join_date, '%Y') AS year FROM friends"
 	friends = mysql.query_db(query)
 	return	render_template("index.html", all_friends=friends)
 
+
+@app.route('/new')
+def newuser():
+	return	render_template('new.html')
+
 @app.route('/friends', methods=['POST'])
 def post():
-	name = request.form['name']
-	age = request.form['age']
 	query = "INSERT INTO friends (friends.name,friends.age,friends.join_date) VALUES (:name, :age, NOW())"
 	
 	data = {
@@ -27,6 +30,58 @@ def post():
 
 	return redirect('/')
 
+@app.route('/<id>')
+def user(id):
+	query = """SELECT friends.id, friends.name, friends.age, 
+	DATE_FORMAT(friends.join_date,'%M %d') AS join_date, 
+	DATE_FORMAT(friends.join_date, '%Y') AS year 
+	FROM friends
+	WHERE friends.id = :id"""
+
+	data ={
+	"id":id
+	}
+
+	friends = mysql.query_db(query, data)
+
+	print friends
+	return render_template('user.html', all_friends=friends[0])
+
+@app.route('/<id>/edit')
+def edit(id):
+	user_id = id
+	return render_template('edit.html', id = user_id)
+	
+@app.route('/<id>/edit/alter', methods = ["POST"])
+def alterdata(id):
+	query = """UPDATE friends
+	SET friends.name = :new_name, friends.age = :new_age
+	WHERE friends.id = :id"""
+
+	data = {
+	"new_name": request.form['name'],
+	"new_age": request.form['age'],
+	"id" : id
+	}
+
+	print data
+
+	mysql.query_db(query, data)
+
+	return redirect('/')
+
+@app.route('/<id>/delete')
+def delete(id):
+	query ="""DELETE FROM friends
+	WHERE friends.id = :id """
+	data = {
+	"id": id
+	}
+
+	mysql.query_db(query, data)
+
+	return	redirect('/')
 
 
+	
 app.run(debug=True)
